a22701921da1dc6cca0423cc1bdf9ed61d19cfe8
Mahusaa <usamah.hafizh@ui.ac.id>
2024-03-13 11:59:32 +0700

    fix search(add button doesnt work)

--- src/App.tsx
+++ src/App.tsx
@@ -18,52 +18,17 @@ const App: React.FC = () => {
   const [amountIsValid, setAmountIsValid] = useState<boolean>(true);
   const [showCartAnimation, setShowCartAnimation] = useState<boolean>(false);
 
-
-  // const handlePurchase = (cashierId: string, purchaseQuantities: { [foodId: string]: number }) => {
-  //   const updatedCashiers = cashiers.map(cashier => {
-  //     if (cashier.id === cashierId) {
-  //       const updatedFoods = cashier.foods.map(food => {
-  //         const quantityPurchased = purchaseQuantities[food.id] || 0;
-  //         if (quantityPurchased > 0 && food.stock >= quantityPurchased) {
-  //           return { ...food, stock: food.stock - quantityPurchased };
-  //         }
-  //         return food;
-  //       });
-  //       return { ...cashier, foods: updatedFoods };
-  //     }
-  //     return cashier;
-  //   });
-  //   setCashiers(updatedCashiers);
-
-  //   setPurchaseQuantities(prevState => ({ ...prevState, [cashierId]: {} }))
-  // };
   console.log(cartItems);
   console.log(totalAmount);
   const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearchQuery(e.target.value);
   };
 
-  const submitHandler = (e: React.FormEvent<HTMLFormElement>, cashierId: string, foodId: string) => {
-    e.preventDefault();
-
-    if (!amountInputRef.current) return;
-
-    const enteredAmount = amountInputRef.current.value;
-    const enteredAmountToNumber = +enteredAmount;
-
-    if (
-      enteredAmount.trim().length === 0 ||
-      enteredAmountToNumber < 1 ||
-      enteredAmountToNumber > 5
-    ) {
-      setAmountIsValid(false);
-      return;
-    }
-
+  const handleAnimation = (cashierId: string, foodId: string) => {
     addToCart(cashierId, foodId);
-    setShowCartAnimation(true); // Set animation to true when adding to cart
+    setShowCartAnimation(true);
     setTimeout(() => {
-      setShowCartAnimation(false); // Reset animation after a delay
+      setShowCartAnimation(false);
     }, 100);
   };
   const calculateTotalQuantity = (): number => {
@@ -83,7 +48,7 @@ const App: React.FC = () => {
         <h1 className="text-3xl font-bold">Cashier Dashboard</h1>
         <div className="flex">
           <SearchBar value={searchQuery} onChange={handleSearch} />
-          <DisplayCart showCartAnimation={showCartAnimation} calculateTotalQuantity={calculateTotalQuantity} cashiers={FoodData}/>
+          <DisplayCart showCartAnimation={showCartAnimation} calculateTotalQuantity={calculateTotalQuantity} cashiers={FoodData} />
         </div>
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
@@ -104,76 +69,17 @@ const App: React.FC = () => {
                           <p className="text-gray-500">Price: Rp. {food.price}</p>
                         </div>
                         <div className="flex flex-col">
-                          <form onSubmit={(e) => submitHandler(e, cashier.id, food.id)}>
-                            <input
-                              id={"amount_" + cashier.id}
-                              type="number"
-                              min="1"
-                              max="5"
-                              step="1"
-                              defaultValue="1"
-                              ref={amountInputRef}
-                            />
-                            {/* Pass cashierId and foodId as data attributes */}
-                            <Button
-                              data-cashier-id={cashier.id}
-                              data-food-id={food.id}
-                              type="submit" // Ensure the button submits the form
-                            >
-                              + Add
-                            </Button>
-                          </form>
-                          {/* <input
-                            type="number"
-                            placeholder='0'
-                            value={(purchaseQuantities[cashier.id] && purchaseQuantities[cashier.id][food.id])}
-                            onChange={(e) => handleQuantityChange(cashier.id, food.id, parseInt(e.target.value))}
-                            className="py-1 px-2 mx-2 text-center border border-gray-300 rounded-md w-16"
-                            min={0}
-                          />
-                          <div className="px-2 py-1">
-                            <Button className='text-sm'>
-                              + Add
-                            </Button>
-                          </div> */}
+                          <Button
+                            onClick={(e) => handleAnimation(cashier.id, food.id)}
+                          >
+                            + Add
+                          </Button>
                         </div>
-
                       </li>
                     ))}
                   </ul>
                 </ScrollArea>
               </CardContent>
-              {/* <div className="flex justify-between items-center px-4 py-2 bg-gray-100">
-                <p className="font-semibold">Harus Bayar: Rp. {calculateTotalPrice()}</p>
-                <Dialog>
-                  <DialogTrigger><Button>Konfirmasi</Button></DialogTrigger>
-                  <DialogContent>
-                    <DialogHeader>
-                      <DialogTitle>
-                        Apakah kamu yakin?
-                      </DialogTitle>
-                      <DialogDescription>
-                        <p className="font-semibold">Cashier: {cashier.name}</p>
-                        {Object.entries(purchaseQuantities[cashier.id] || {}).map(([foodId, quantity]) => {
-                          if (isNaN(quantity) || quantity === 0) return null; // Skip rendering if quantity is NaN or 0
-                          const food = cashier.foods.find(food => food.id === foodId);
-                          if (!food) return null;
-                          return (
-                            <div key={food.id} className="flex justify-between">
-                              <p>{food.name} - {quantity} pcs</p>
-                              <p>Rp. {quantity * food.price}</p>
-                            </div>
-                          );
-                        })}
-                        <p className="font-semibold">Total: Rp. {calculateTotalPrice()}</p>
-                      </DialogDescription>
-                    </DialogHeader>
-                    <DialogClose>
-                      <Button onClick={() => handlePurchase(cashier.id, purchaseQuantities[cashier.id] || {})} className="px-4 py-2  text-white rounded-md"> Konfirddddmasi </Button>
-                    </DialogClose>
-                  </DialogContent>
-                </Dialog>
-              </div> */}
             </Card>
           </div>
         ))}
