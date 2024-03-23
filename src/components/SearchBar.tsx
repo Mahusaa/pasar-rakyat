0e3cef3cc87ffec11ca3ad0c5b46398c695d1eb5
Mahusaa <usamah.hafizh@ui.ac.id>
2024-03-23 08:01:45 +0700

    add logo to search bar

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,22 +1,24 @@
-import React from "react"
+import React from "react";
+import SearchIcon from "./icons/SearchIcon";
 
 interface SearchBarProps {
-    value: string,
-    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void,
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
 const SearchBar: React.FC<SearchBarProps> = ({ value, onChange }) => {
-    return (
-        <div >
-            <input
-                type="text"
-                placeholder="Search"
-                className="px-4 py-2 border rounded-md w-full"
-                value={value}
-                onChange={onChange}
-            />
-        </div>
-    )
-}
+  return (
+    <div className="flex items-center border rounded-md px-3 py-2 bg-white shadow-sm">
+      <SearchIcon className="w-6 h-6 mr-2 text-gray-400" />
+      <input
+        type="text"
+        placeholder="Search"
+        className="flex-1 focus:outline-none focus:border-blue-500"
+        value={value}
+        onChange={onChange}
+      />
+    </div>
+  );
+};
 
 export default SearchBar;
