9e5e21f2d6eb6c0b56bf00b8f48d293a1c9181f8
Mahusaa <usamah.hafizh@ui.ac.id>
2024-03-23 08:00:30 +0700

    add  websocket to the backend

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,42 @@
+require('dotenv').config({ path: './backend/.env' });
 const express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
 const bodyParser = require('body-parser');
+const firebaseApp = require('./config/firebaseConfig');
 const routes = require('./routes/stock');
 
+
+
 const app = express();
+const server = http.createServer(app); // Create an HTTP server
+const wss = new WebSocket.Server({ server }); // Create a WebSocket server
+
 
 app.use(bodyParser.json());
 app.use('/api', routes);
 
+// Handle WebSocket connections
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+
+  // Create a Firebase listener for your data
+  const db = firebaseApp.database();
+  const menuRef = db.ref('menu');
+
+  menuRef.on('value', (snapshot) => {
+    const menuData = snapshot.val();
+    ws.send(JSON.stringify(menuData));
+  });
+
+  ws.on('close', () => {
+    // Remove Firebase listener when client disconnects
+    menuRef.off();
+    console.log('Client disconnected');
+  });
+});
+
 const PORT = 8080;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
